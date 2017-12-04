9634ec1992f090bd4ae86c9fae8c920d2de42f7a
IgorPerikov <perikov.igor@gmail.com>
2017-12-04 08:58:18 +0300

    day 4

---
+++ 2017/day4/p2.py
@@ -0,0 +1,15 @@
+import collections
+
+with open('input') as f:
+    content = f.readlines()
+content = [x.strip('\n') for x in content]
+valid_count = 0
+for pwd in content:
+    pwd_words = pwd.split(' ')
+    counter = collections.Counter()
+    for pwd_word in pwd_words:
+        counter[''.join(sorted(pwd_word))] += 1
+    word, count = counter.most_common(1)[0]
+    if count == 1:
+        valid_count += 1
+print(valid_count)
