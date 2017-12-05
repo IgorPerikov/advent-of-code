b6ae8ad1311ad6e222096889a6b40a9018630ce0
Igor Perikov <IgorPerikov@users.noreply.github.com>
2017-12-05 18:06:53 +0300

    Create p2.py

---
+++ 2017/day5/p2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+with open('input') as f:
+    content = f.readlines()
+content = [int(x.strip('\n')) for x in content]
+count = 0
+i = 0
+while True:
+    try:
+        step = content[i]
+        if step >= 3:
+            content[i] -= 1
+        else:
+            content[i] += 1
+        i += step
+        count += 1
+    except:
+        break
+print(count)
